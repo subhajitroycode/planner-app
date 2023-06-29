6aedf02c4680fe466502f0bf32cfaf768ea9b0ed
Subhajit Roy <roysubhajit8697@gmail.com>
2023-06-29 21:28:43 +0530

    fixed components folder name

--- src/widgets/CalculatorWidget.js
+++ src/widgets/CalculatorWidget.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "../componets/Button";
+import Button from "../components/Button";
 
 export default function CalculatorWidget() {
   const [display, setDisplay] = useState("0");
